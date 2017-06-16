b509f3f21b31afb85a6aa21cf4475f16510f4c55
Joshua Kaden <jkaden@doitt.nyc.gov>
2017-06-16 13:06:18 -0400

    create xcode project

---
+++ CirculateSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CirculateSwift
+//
+//  Created by Kaden, Joshua on 6/16/17.
+//  Copyright © 2017 Chadford Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
