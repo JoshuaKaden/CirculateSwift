7ba82e09195ff5007df0b266a8fd8d092b74d373
JoshuaKaden <jkaden@kadenspace.com>
2017-07-08 11:08:41 -0400

    only calculate delta if we need to

--- CirculateSwift/ArteryView.swift
+++ CirculateSwift/ArteryView.swift
@@ -103,14 +103,13 @@ final class ArteryView: UIView {
             targetY = tFrame.origin.y - halfPaddingHeight
         }
         
-        let delta: CGFloat
-        if targetY < path.currentPoint.y {
-            delta = (path.currentPoint.y - targetY) * -1
-        } else {
-            delta = targetY - path.currentPoint.y
-        }
-        
         if let _ = viewModel.originSystem {
+            let delta: CGFloat
+            if targetY < path.currentPoint.y {
+                delta = (path.currentPoint.y - targetY) * -1
+            } else {
+                delta = targetY - path.currentPoint.y
+            }
             path.addLine(to: CGPoint(x: path.currentPoint.x, y: path.currentPoint.y + delta))
         } else {
             path.move(to: CGPoint(x: dataSource.aortaX, y: targetY))
