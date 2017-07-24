8d6df6f6249dc898d15f81a198bc7de3819d711e
JoshuaKaden <jkaden@kadenspace.com>
2017-07-23 23:11:31 -0400

    …more

--- CirculateSwift/UIView+ContainsPoint.swift
+++ CirculateSwift/UIView+ContainsPoint.swift
@@ -6,4 +6,44 @@
 //  Copyright © 2017 Chadford Software. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    func containsPoint(_ point: CGPoint, path: UIBezierPath, inFillArea: Bool) -> Bool {
+        return containsPoints([point], path: path, inFillArea: inFillArea)
+    }
+    
+    // From JaredH's solution on https://stackoverflow.com/questions/22691891/detecting-tap-inside-a-bezier-path
+    func containsPoints(_ points: [CGPoint], path: UIBezierPath, inFillArea: Bool) -> Bool {
+        UIGraphicsBeginImageContext(self.size)
+        guard let context = UIGraphicsGetCurrentContext() else { return false }
+        
+        let pathToTest = path.cgPath
+        
+        var mode: CGPathDrawingMode = CGPathDrawingMode.stroke
+        
+        if inFillArea {
+            // check if UIBezierPath uses EO fill
+            if path.usesEvenOddFillRule {
+                mode = CGPathDrawingMode.eoFill
+            } else {
+                mode = CGPathDrawingMode.fill
+            }
+        } // else mode == stroke
+        
+        context.saveGState()
+        context.addPath(pathToTest)
+        
+        for point in points {
+            if context.pathContains(point, mode: mode) {
+                context.restoreGState()
+                return true
+            }
+        }
+        
+        context.restoreGState()
+        UIGraphicsEndImageContext()
+        
+        return false
+    }
+}
